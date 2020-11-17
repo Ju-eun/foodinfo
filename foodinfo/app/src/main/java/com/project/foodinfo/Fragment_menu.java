b885e15dec4cc646cd3554d3ec25a00211a7aa17
kcg0602s <kcg0602s@naver.com>
2020-11-17 18:13:22 +0900

    영훈
    가게정보

--- foodinfo/app/src/main/java/com/project/foodinfo/Fragment_menu.java
+++ foodinfo/app/src/main/java/com/project/foodinfo/Fragment_menu.java
@@ -1,4 +1,4 @@
-package com.project.foodinfo.Sign;
+package com.project.foodinfo;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,8 +6,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-
-import com.project.foodinfo.R;
 
 /**
  * A simple {@link Fragment} subclass.
