d8e4663f3f275096c7fb34eb4df1d7ed173c863f
kcg0602s <kcg0602s@naver.com>
2020-11-18 14:32:50 +0900

    영훈

--- foodinfo/app/src/main/java/com/project/foodinfo/Fragment_menu.java
+++ foodinfo/app/src/main/java/com/project/foodinfo/Fragment_menu.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -20,10 +23,29 @@ public class Fragment_menu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        ListView lv_menu;
+
+        View view;
+
+        view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+
+        lv_menu = (ListView) view.findViewById(R.id.lv_menu);
+        MyAdapter myAdapter = new MyAdapter();
+        for(int i = 0;i<10;i++){
+            myAdapter.addItem(R.drawable.iu,"국밥","1000");
+            myAdapter.notifyDataSetChanged();
+        }
+        lv_menu.setAdapter(myAdapter);
+
+
+
+
+        return view;
+    }
+
 
 
     }
 
 
-}
