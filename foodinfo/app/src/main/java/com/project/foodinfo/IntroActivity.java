378f76393b8a70e1a4f440ebcf73b1406d78ab30
rlawjdals96 <kimjungmin__@naver.com>
2020-11-19 11:28:18 +0900

    진순아 이걸로 ㄱㄱ

--- foodinfo/app/src/main/java/com/project/foodinfo/IntroActivity.java
+++ foodinfo/app/src/main/java/com/project/foodinfo/IntroActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.airbnb.lottie.LottieAnimationView;
 
 public class IntroActivity extends AppCompatActivity {
 
